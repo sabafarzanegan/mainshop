be6a2222814ec428007ccb1b7afa4efa33c4e536
saba farzanegan <saba.far.99@gmail.com>
2024-09-14 14:29:55 +0330

    add products to home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,67 @@
-export default function Home() {
+import { eq } from "drizzle-orm";
+import { db } from "../db/drizzle";
+import {
+  products,
+  productVariants,
+  variantImages,
+  variantTags,
+} from "../db/schema";
+import ProductSection from "../components/main/product/ProductSection";
+export default async function Home() {
+  const allProducts = await db
+    .select()
+    .from(products)
+    .fullJoin(productVariants, eq(products.id, productVariants.productID))
+    .fullJoin(variantImages, eq(productVariants.id, variantImages.variantID))
+    .fullJoin(variantTags, eq(productVariants.id, variantTags.variantID));
+
+  const organizedVariants = [];
+
+  allProducts.forEach((item) => {
+    let existingProduct = organizedVariants.find(
+      (variant) => variant.products.id === item.products.id
+    );
+
+    if (existingProduct) {
+      if (item.productVariants) {
+        const existingVariant = existingProduct.productVariants.find(
+          (variant) => variant.id === item.productVariants.id
+        );
+        if (!existingVariant) {
+          existingProduct.productVariants.push(item.productVariants);
+        }
+      }
+
+      if (item.variantImages) {
+        const existingImage = existingProduct.variantImages.find(
+          (image) => image.id === item.variantImages.id
+        );
+        if (!existingImage) {
+          existingProduct.variantImages.push(item.variantImages);
+        }
+      }
+
+      if (item.variantTags) {
+        const existingTag = existingProduct.variantTags.find(
+          (tag) => tag.id === item.variantTags.id
+        );
+        if (!existingTag) {
+          existingProduct.variantTags.push(item.variantTags);
+        }
+      }
+    } else {
+      organizedVariants.push({
+        products: item.products,
+        productVariants: item.productVariants ? [item.productVariants] : [],
+        variantImages: item.variantImages ? [item.variantImages] : [],
+        variantTags: item.variantTags ? [item.variantTags] : [],
+      });
+    }
+  });
+
   return (
-    <main className="flex flex-col items-center justify-between p-24"></main>
+    <main className="my-2">
+      <ProductSection data={organizedVariants} />
+    </main>
   );
 }
---
+++ components/main/product/ProductSection.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { Card, CardContent, CardHeader, CardTitle } from "../../ui/card";
+import Image from "next/image";
+import { Badge } from "../../ui/badge";
+function ProductSection({ data }) {
+  console.log(data);
+
+  return (
+    <main className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+      {data.map((product) => (
+        <Link
+          key={product.products.id}
+          href={`/products/${product.products.id}?id=${product.products.id}&productID=${product.productVariants[0].id}&price=${product.products.price}`}>
+          <Card className="">
+            <CardHeader>
+              <Image
+                width={620}
+                height={480}
+                src={product.variantImages[0].url}
+                alt={product.variantImages[0].name}
+                loading="lazy"
+              />
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <div className="space-y-3">
+                <h2 className="font-semibold text-base">
+                  {product.products.title}
+                </h2>
+                <h5 className="text-sm">
+                  <span className="">نوع محصول:</span>
+                  {product.productVariants[0].productType}
+                </h5>
+              </div>
+              <div className="">
+                <Badge variant="secondary" className="text-base">
+                  {product.products.price.toLocaleString("fa")}تومان
+                </Badge>
+              </div>
+            </CardContent>
+          </Card>
+        </Link>
+      ))}
+    </main>
+  );
+}
+
+export default ProductSection;
